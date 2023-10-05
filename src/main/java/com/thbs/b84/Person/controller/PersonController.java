06246296fd6b6abd1b2c4cd135aabcf579507773
prasannabalajitp <61289440+prasannabalajitp@users.noreply.github.com>
2023-10-05 23:16:33 +0530

    Update PersonController.java

--- src/main/java/com/thbs/b84/Person/controller/PersonController.java
+++ src/main/java/com/thbs/b84/Person/controller/PersonController.java
@@ -1,9 +1,10 @@
-package com.thbs.b84.controller;
+package com.thbs.b84.Person.controller;
 
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,29 +16,44 @@ import com.thbs.b84.repository.PersonRepository;
 
 
 @RestController
-@RequestMapping("/persons")
+//@RequestMapping("/persons")
 public class PersonController {
 
-	@GetMapping("/getAllPersonDetails")
-	public ResponseEntity <List<Person>>getAllPersonDetails()
-	{
-	
-		PersonRepository pr = new PersonRepository();
-		List<Person> plist = pr.getAllPersonDetails();
-		return new ResponseEntity<List<Person>>(plist, HttpStatus.OK);
-	}
-	@GetMapping("/getAPerson/{id}")
-	public ResponseEntity<Person> getBookById(@PathVariable int id)
-	{
-		PersonRepository br = new PersonRepository();
-		Person b = br.getAPerson(id);
-		if(b!=null)
-		{
-			return new ResponseEntity<Person>(b,HttpStatus.OK);
-		}
-		else
-		{
-			return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
-		}
-	}
+        @RequestMapping("/")
+        public String displayWebPage(){
+                return "Hello from Docker";
+    }
+        @RequestMapping("/hellowebpage")
+        @ResponseBody
+        public String displayHello(){
+                return "index";
+        }
+
+        @GetMapping("/getAllPersonDetails")
+        public ResponseEntity <List<Person>>getAllPersonDetails()
+        {
+
+                PersonRepository pr = new PersonRepository();
+                List<Person> plist = pr.getAllPersonDetails();
+                System.out.println(plist);
+                return new ResponseEntity<List<Person>>(plist, HttpStatus.OK);
+        }
+        @GetMapping("/getAPerson/{id}")
+        public ResponseEntity<Person> getBookById(@PathVariable int id)
+        {
+                PersonRepository br = new PersonRepository();
+                Person b = br.getAPerson(id);
+                if(b!=null)
+                {
+                        return new ResponseEntity<Person>(b,HttpStatus.OK);
+                }
+                else
+                {
+                        return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
+                }
+        }
+        @GetMapping("/hello")
+        public String hello(){
+        return "Hello !!!" ;
+    }
 }
